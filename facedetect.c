8be59da15b2658e76fbd611d55aab1c419ee8d6a
Justin Dubs <jtdubs@gmail.com>
2009-06-20 12:27:42 -0400

    more python

--- facedetect.c
+++ facedetect.c
@@ -22,6 +22,7 @@ int main() {
         cvClearMemStorage(storage);
 
         CvSeq* faces = cvHaarDetectObjects(small, cascade, storage, 1.1, 2, CV_HAAR_DO_CANNY_PRUNING, cvSize(30, 30));
+
         if (faces) {
             CvRect r = *(CvRect*)cvGetSeqElem(faces, 0);
             cvRectangle(frame, cvPoint(r.x*scale,r.y*scale), cvPoint((r.x+r.width)*scale, (r.y+r.height)*scale), CV_RGB(255,0,0), 3, 8, 0);
--- facedetect.py
+++ facedetect.py
@@ -5,7 +5,7 @@ from CVtypes import *
 scale = 1.3
 
 def main():
-    cascade = cv.LoadHaarClassifierCascade('/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml', cv.Size(1,1))
+    cascade = cv.LoadHaarClassifierCascade('/usr/share/opencv/haarcascades/haarcascade_frontalface_alt2.xml', cv.Size(1,1))
     storage = cvCreateMemStorage(0)
     capture = cvCreateCameraCapture(0)
     frame   = cvQueryFrame(capture)
@@ -23,8 +23,7 @@ def main():
         cvClearMemStorage(storage)
 
         faces = cvHaarDetectObjects(small, cascade, storage, 1.1, 2, CV_HAAR_DO_CANNY_PRUNING, CvSize(30, 30))
-        if faces:
-            r = faces[0]
+        for r in faces:
             cvRectangle(frame, CvPoint(int(r.x*scale), int(r.y*scale)), CvPoint(int((r.x+r.width)*scale), int((r.y+r.height)*scale)), CV_RGB(255, 0, 0), 3, 8, 0)
 
         cvShowImage('result', frame)
