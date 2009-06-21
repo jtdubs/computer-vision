c500b51622920f8ec0c463093e6962a3083e622b
Justin Dubs <jtdubs@gmail.com>
2009-06-20 20:45:04 -0400

    start feature tracking

--- facedetect.py
+++ facedetect.py
@@ -10,24 +10,40 @@ def main():
     capture = cvCreateCameraCapture(0)
     frame   = cvQueryFrame(capture)
     size    = cvGetSize(frame)
+    roi     = cvCloneImage(frame)
     gray    = cvCreateImage(size, 8, 1)
     small   = cvCreateImage(CvSize(int(size.width/scale), int(size.height/scale)), 8, 1)
+    eigs    = cvCreateImage(CvSize(int(size.width/scale), int(size.height/scale)), 32, 1)
+    temp    = cvCreateImage(CvSize(int(size.width/scale), int(size.height/scale)), 32, 1)
+    cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH,  640)
+    cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, 480)
 
+    cvNamedWindow('frame', 1)
+    cvNamedWindow('roi',   1)
 
-    cvNamedWindow('result', 1)
-
+    state = 'find_face'
     while True:
-        cvCvtColor(frame, gray, CV_BGR2GRAY)
-        cvResize(gray, small, CV_INTER_LINEAR)
-        cvEqualizeHist(small, small)
-        cvClearMemStorage(storage)
+        if state == 'find_face':
+            cvCopy(frame, roi, None)
+            cvCvtColor(frame, gray, CV_BGR2GRAY)
+            cvResize(gray, small, CV_INTER_LINEAR)
+            cvEqualizeHist(small, small)
+            cvClearMemStorage(storage)
 
-        faces = cvHaarDetectObjects(small, cascade, storage, 1.1, 2, CV_HAAR_DO_CANNY_PRUNING, CvSize(30, 30))
-        for r in faces:
-            cvRectangle(frame, CvPoint(int(r.x*scale), int(r.y*scale)), CvPoint(int((r.x+r.width)*scale), int((r.y+r.height)*scale)), CV_RGB(255, 0, 0), 3, 8, 0)
+            faces = cvHaarDetectObjects(small, cascade, storage, 1.1, 2, CV_HAAR_DO_CANNY_PRUNING, CvSize(30, 30))
+            if not faces: continue
 
-        cvShowImage('result', frame)
-        if cvWaitKey(10) >= 0: break
+            r = faces[0]
+            tl = CvPoint(int(r.x*scale), int(r.y*scale))
+            br = CvPoint(int((r.x+r.width)*scale), int((r.y+r.height)*scale))
+            cvSetImageROI(roi, CvRect(tl.x, tl.y, br.x-tl.x, br.y-tl.y))
+            cvRectangle(frame, tl, br, CV_RGB(255, 0, 0), 3, 8, 0)
+
+        cvShowImage('frame', frame)
+        cvShowImage('roi',   roi)
+
+        if cvWaitKey(10) == 0x1B:
+            break
 
         frame = cvQueryFrame(capture)
 
