ba34e1269c990d6270dbe612148d4725b5fc23bb
Justin Dubs <jtdubs@gmail.com>
2009-06-25 19:12:43 -0400

    track small moving average of head position

--- facedetect.py
+++ facedetect.py
@@ -1,60 +1,101 @@
 #!/usr/bin/python
 
+from OpenGL.GLUT import *
+from OpenGL.GLU  import *
+from OpenGL.GL   import *
+
 from opencv         import *
 from opencv.highgui import *
 
+import sys
+
 scale = 1.3
 
+class FaceTracking:
+    def __init__(self):
+        self.initGlut()
+        self.initCV()
+        self.initTracker()
+
+    def initGlut(self):
+        glutInit(sys.argv)
+        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
+        glutInitWindowSize(640,480)
+        glutCreateWindow('Face Recognition')
+
+        glClearColor(0.0, 0.0, 0.0, 1.0)
+        glShadeModel(GL_SMOOTH)
+        glutDisplayFunc(self.on_display)
+
+    def initCV(self):
+        self.cascade = cvLoadHaarClassifierCascade('/usr/share/opencv/haarcascades/haarcascade_frontalface_alt2.xml', cvSize(1, 1))
+        self.storage = cvCreateMemStorage(0)
+        self.capture = cvCaptureFromCAM(0)
+        self.frame   = cvQueryFrame(self.capture)
+        self.size    = cvSize(self.frame.width, self.frame.height)
+        self.gray    = cvCreateImage(self.size, 8, 1)
+        self.small   = cvCreateImage(CvSize(int(self.size.width/scale), int(self.size.height/scale)), 8, 1)
+        self.eigs    = cvCreateImage(CvSize(int(self.size.width/scale), int(self.size.height/scale)), 32, 1)
+        self.temp    = cvCreateImage(CvSize(int(self.size.width/scale), int(self.size.height/scale)), 32, 1)
+
+        cvNamedWindow('frame', 1)
+        cvNamedWindow('small', 1)
+
+    def initTracker(self):
+        self.state   = 'find_face'
+        self.history = (None, None, None, None, None)
+
+    def cleanupCV(self):
+        cvDestroyWindow('result')
+        cvReleaseCapture(self.capture)
+        cvReleaseImage(self.gray)
+        cvReleaseImage(self.small)
+
+    def main(self):
+        while True:
+            self.frame = cvQueryFrame(self.capture)
+
+            if self.state == 'find_face':
+                cvCvtColor(self.frame, self.gray, CV_BGR2GRAY)
+                cvResize(self.gray, self.small, CV_INTER_LINEAR)
+                cvEqualizeHist(self.small, self.small)
+                cvClearMemStorage(self.storage)
+
+                faces = cvHaarDetectObjects(self.small, self.cascade, self.storage, 1.1, 2, CV_HAAR_DO_CANNY_PRUNING, cvSize(30, 30))
+                if faces:
+                    r = faces[0]
+                    tl = cvPoint(int(r.x*scale), int(r.y*scale))
+                    br = cvPoint(int((r.x+r.width)*scale), int((r.y+r.height)*scale))
+                    cvRectangle(self.frame, tl, br, CV_RGB(255, 0, 0), 3, 8, 0)
+                    self.history = self.history[1:] + (r,)
+
+                cvShowImage('small', self.small)
+
+            cvShowImage('frame', self.frame)
+
+            if cvWaitKey(10) == 0x1B:
+                break
+
+            glutPostRedisplay()
+            glutMainLoopEvent()
+
+        self.cleanupCV()
+
+    def on_display(self):
+        x, y, s, c = 0, 0, 0, 0
+        for r in self.history:
+            if r:
+                x = x + r.x + (r.width/2)
+                y = y + r.y + (r.height/2)
+                s = s + ((r.height + r.width)/2)
+                c = c + 1
+        x, y, s = x / c, y / c, s / c
+        print x, y, s
+        glClear(GL_COLOR_BUFFER_BIT)
+        glutSwapBuffers()
+
 def main():
-    cascade = cvLoadHaarClassifierCascade('/usr/share/opencv/haarcascades/haarcascade_frontalface_alt2.xml', cvSize(1, 1))
-    storage = cvCreateMemStorage(0)
-    capture = cvCaptureFromCAM(0)
-    frame   = cvQueryFrame(capture)
-    size    = cvSize(frame.width, frame.height)
-    gray    = cvCreateImage(size, 8, 1)
-    small   = cvCreateImage(CvSize(int(size.width/scale), int(size.height/scale)), 8, 1)
-    eigs    = cvCreateImage(CvSize(int(size.width/scale), int(size.height/scale)), 32, 1)
-    temp    = cvCreateImage(CvSize(int(size.width/scale), int(size.height/scale)), 32, 1)
-
-    cvNamedWindow('frame', 1)
-    cvNamedWindow('small', 1)
-
-    state = 'find_face'
-    while True:
-        frame = cvQueryFrame(capture)
-
-        if state == 'find_face':
-            cvCvtColor(frame, gray, CV_BGR2GRAY)
-            cvResize(gray, small, CV_INTER_LINEAR)
-            cvEqualizeHist(small, small)
-            cvClearMemStorage(storage)
-
-            faces = cvHaarDetectObjects(small, cascade, storage, 1.1, 2, CV_HAAR_DO_CANNY_PRUNING, cvSize(30, 30))
-            if faces:
-                r = faces[0]
-                tl = cvPoint(int(r.x*scale), int(r.y*scale))
-                br = cvPoint(int((r.x+r.width)*scale), int((r.y+r.height)*scale))
-                cvRectangle(frame, tl, br, CV_RGB(255, 0, 0), 3, 8, 0)
-
-                cvSetImageROI(small, r)
-                cvSetImageROI(eigs,  r)
-                cvSetImageROI(temp,  r)
-                for p in cvGoodFeaturesToTrack(small, eigs, temp, None, 100, 0.02, 4.0, use_harris=False):
-                    cvCircle(frame, cvPoint(int((r.x+p.x)*scale), int((r.y+p.y)*scale)), 2, CV_RGB(0, 0, 255), 1)
-                cvResetImageROI(small)
-                cvResetImageROI(eigs)
-                cvResetImageROI(temp)
-
-        cvShowImage('small', small)
-        cvShowImage('frame', frame)
-
-        if cvWaitKey(10) == 0x1B:
-            break
-
-    cvReleaseImage(gray)
-    cvReleaseImage(small)
-    cvReleaseCapture(capture)
-    cvDestroyWindow('result')
+    FaceTracking().main()
 
 if __name__ == '__main__':
     main()
