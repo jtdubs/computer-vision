6780c45251fbf8924005e57ff6fbd43bb353dd20
Justin Dubs <jtdubs@gmail.com>
2009-06-27 01:57:03 -0400

    use feature tracking instead of smoothed history of face positions

--- facedetect.py
+++ facedetect.py
@@ -21,7 +21,7 @@ class FaceTracking:
     def init_glut(self):
         glutInit(sys.argv)
         glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
-        glutInitWindowSize(640,480)
+        glutInitWindowSize(640, 480)
         glutCreateWindow('Face Recognition')
 
         glClearColor(0.0, 0.0, 0.0, 1.0)
@@ -45,26 +45,16 @@ class FaceTracking:
         self.features = None
 
         cvNamedWindow('frame', 1)
-        cvNamedWindow('small', 1)
 
     def init_tracker(self):
         self.state   = 'find_face'
-        self.history = (None,) * self.window
+        self.flags   = 0
+        self.x, self.y, self.spread, self.distance = 0, 0, 0, 0
 
     def cleanup_cv(self):
         cvDestroyWindow('result')
 
-    def rect_to_params(self, rect):
-        anglePerPixel = (3.14159 / 4.5) / 480.0
-        angle         = rect.width * anglePerPixel
-        distance      = (0.12/2.0) / tan(angle/2.0)
-        x             = (320 - (rect.x + (rect.width  / 2.0))) / 160.0 * distance
-        y             = (240 - (rect.y + (rect.height / 2.0))) / 120.0 * distance
-        return (x, y, rect.height, distance)
-
     def on_reshape(self, w, h):
-        print "viewport:", w, h
-
         glViewport(0, 0, w, h)
 
         glMatrixMode(GL_PROJECTION)
@@ -75,19 +65,11 @@ class FaceTracking:
         glLoadIdentity()
 
     def on_display(self):
-        x, y, d, c = 0, 0, 0, 0
-        for (x1, y1, h1, d1) in [h for h in self.history if h]:
-            x, y, d, c = x+x1, y+y1, d+d1, c+1
-        if c > 0:
-            x, y, d = x / c, y / c, d / c
-
-        print x, y, d
-
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
         glLoadIdentity()
         glTranslatef(0.0, 0.0, -5.0)
-        gluLookAt(x*2, y*2, abs(d*6), 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+        gluLookAt(self.x*2, self.y*2, abs(self.distance*6), 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
         glBegin(GL_QUADS)
         for (x, y) in [(0, 0), (3, 1), (-1, -2), (-3, 3), (2, -3)]:
             glColor3f(0.0, 0.0, 1.0); glVertex3f( 0.1+x,  0.1+y, -2.0); glVertex3f(-0.1+x,  0.1+y, -2.0); glVertex3f(-0.1+x,  0.1+y,  2.0); glVertex3f( 0.1+x,  0.1+y,  2.0)
@@ -110,63 +92,58 @@ class FaceTracking:
             cvRectangle(frame, CvPoint(r.x, r.y), CvPoint(r.x+r.width, r.y+r.height), CV_RGB(255, 0, 0), 3, 8, 0)
 
         if best:
-            self.history = self.history[1:] + (self.rect_to_params(best),)
-
             cvSetImageROI(self.gray, best)
             cvSetImageROI(self.eigs, best)
             cvSetImageROI(self.temp, best)
 
             self.features = [x for x in cvGoodFeaturesToTrack(self.gray, self.eigs, self.temp, None, 20, 0.02, 4.0, use_harris=False)]
-            print "identified features:", len(self.features)
+            min_x, max_x, = 1000, 0
             for f in self.features:
-                f.x = f.x + best.x
-                f.y = f.y + best.y
+                f.x, f.y = f.x + best.x, f.y + best.y
+                min_x, max_x = min(min_x, f.x), max(max_x, f.x)
                 cvCircle(frame, cvPoint(int(f.x), int(f.y)), 3, CV_RGB(0, 0, 255), 1)
 
             cvResetImageROI(self.gray)
             cvResetImageROI(self.eigs)
             cvResetImageROI(self.temp)
 
-            self.state = 'track_face'
+            anglePerPixel = (3.14159 / 4.5) / 480.0
+            angle         = best.width * anglePerPixel
+
+            self.distance = (0.12/2.0) / tan(angle/2.0)
+            self.x        = (320 - (best.x + (best.width  / 2.0))) / 160.0 * self.distance
+            self.y        = (240 - (best.y + (best.height / 2.0))) / 120.0 * self.distance
+            self.spread   = max_x - min_x
+            self.flags    = 0
+            self.state    = 'track_face'
 
     def state_track_face(self, frame):
-        (x, y, height, size) = self.history[-1]
-        best = None
+        features, status = cvCalcOpticalFlowPyrLK(self.prev, self.gray, self.pyr_a, self.pyr_b, self.features, None, None, CvSize(50, 50), 3, None, None, cvTermCriteria(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS, 10, 0.03), self.flags)
+        features = [x for x in features]
+        dx, dy, min_x, max_x, = 0, 0, 1000, 0
+        for i in range(0, len(features)):
+            if ord(status[i]) == 0:
+                print "lost:", i
+                features[i] = None
+            else:
+                dx = dx + (self.features[i].x - features[i].x)
+                dy = dy + (self.features[i].y - features[i].y)
+                min_x, max_x = min(min_x, features[i].x), max(max_x, features[i].x)
+                cvCircle(frame, cvPoint(int(features[i].x), int(features[i].y)), 3, CV_RGB(0, 0, 255), 1)
+        features = [x for x in features if x]
+        if len(features) > 0:
+            dx, dy = dx / len(features), dy / len(features)
+        self.features = features
 
-        for face in cvHaarDetectObjects(self.small, self.cascade, self.storage, 1.1, 2, CV_HAAR_DO_CANNY_PRUNING, cvSize(30, 30)):
-            r = CvRect(int(face.x*self.scale), int(face.y*self.scale), int(face.width*self.scale), int(face.height*self.scale))
-            if not best or abs(r.height-height) < abs(best.height-height):
-                best = r
-            cvRectangle(frame, CvPoint(r.x, r.y), CvPoint(r.x+r.width, r.y+r.height), CV_RGB(0, 0, 255), 3, 8, 0)
+        spread = max_x - min_x
+        self.distance  = self.distance * (self.spread / spread)
+        self.x         = self.x + (dx / 160.0 * self.distance)
+        self.y         = self.y + (dy / 120.0 * self.distance)
+        self.flags     = CV_LKFLOW_PYR_A_READY
+        self.spread    = spread
 
-        if best:
-            cvRectangle(frame, CvPoint(best.x, best.y), CvPoint(best.x+best.width, best.y+best.height), CV_RGB(0, 255, 0), 3, 8, 0)
-            self.history = self.history[1:] + (self.rect_to_params(best),)
-
-            features, status = cvCalcOpticalFlowPyrLK(self.prev, self.gray, self.pyr_a, self.pyr_b, self.features, None, None, CvSize(50, 50), 3, None, None, cvTermCriteria(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS, 10, 0.03), CV_LKFLOW_PYR_A_READY)
-            new_features = [x for x in features]
-            dx, dy, max_x, max_y, min_x, min_y = 0, 0, 0, 0, 1000, 1000
-            for i in range(0, len(features)):
-                if ord(status[i]) == 0:
-                    print "lost feature:", i
-                    del new_features[i]
-                else:
-                    dx = dx + (features[i].x - self.features[i].x)
-                    dy = dy + (features[i].y - self.features[i].y)
-                    max_x = max(max_x, features[i].x)
-                    max_y = max(max_y, features[i].y)
-                    min_x = min(min_x, features[i].x)
-                    min_y = min(min_y, features[i].y)
-                    cvCircle(frame, cvPoint(int(features[i].x), int(features[i].y)), 3, CV_RGB(0, 0, 255), 1)
-            if len(new_features) > 0:
-                dx, dy = dx / len(new_features), dy / len(new_features)
-            self.features = new_features
-
-            print "estimated motion:", dx, dy
-
-            if len(self.features) < 5:
-                self.state = 'find_face'
-        else:
+        if len(self.features) < 5:
+            print "noes"
             self.state = 'find_face'
 
     def main(self):
@@ -186,7 +163,6 @@ class FaceTracking:
             cvCopy(self.gray, self.prev)
             self.pyr_a, self.pyr_b = self.pyr_b, self.pyr_a
 
-            cvShowImage('small', self.small)
             cvShowImage('frame', frame)
 
             if cvWaitKey(10) == 0x1B:
