b7dc6fc4ba0f88c626431de5a2578faeb3d2325a
Justin Dubs <jtdubs@gmail.com>
2009-06-27 01:12:06 -0400

    pull out state code into methods

--- facedetect.py
+++ facedetect.py
@@ -41,8 +41,8 @@ class FaceTracking:
         self.small    = cvCreateImage(CvSize(int(frame.width/self.scale), int(frame.height/self.scale)),  8, 1)
         self.eigs     = cvCreateImage(CvSize(frame.width, frame.height), 32, 1)
         self.temp     = cvCreateImage(CvSize(frame.width, frame.height), 32, 1)
-        self.pyra     = cvCreateImage(CvSize(frame.width, frame.height), 32, 1)
-        self.pyrb     = cvCreateImage(CvSize(frame.width, frame.height), 32, 1)
+        self.pyr_a    = cvCreateImage(CvSize(frame.width, frame.height), 32, 1)
+        self.pyr_b    = cvCreateImage(CvSize(frame.width, frame.height), 32, 1)
         self.features = None
 
         cvNamedWindow('frame', 1)
@@ -101,6 +101,75 @@ class FaceTracking:
 
         glutSwapBuffers()
 
+    def state_find_face(self, frame):
+        best = None
+
+        for face in cvHaarDetectObjects(self.small, self.cascade, self.storage, 1.1, 2, CV_HAAR_DO_CANNY_PRUNING, cvSize(30, 30)):
+            r = CvRect(int(face.x*self.scale), int(face.y*self.scale), int(face.width*self.scale), int(face.height*self.scale))
+            if not best or r.height > best.height:
+                best = r
+            cvRectangle(frame, CvPoint(r.x, r.y), CvPoint(r.x+r.width, r.y+r.height), CV_RGB(255, 0, 0), 3, 8, 0)
+
+        if best:
+            self.history = self.history[1:] + (self.rect_to_params(best),)
+
+            cvSetImageROI(self.gray, best)
+            cvSetImageROI(self.eigs, best)
+            cvSetImageROI(self.temp, best)
+
+            self.features = [x for x in cvGoodFeaturesToTrack(self.gray, self.eigs, self.temp, None, 20, 0.02, 4.0, use_harris=False)]
+            print "identified features:", len(self.features)
+            for f in self.features:
+                f.x = f.x + best.x
+                f.y = f.y + best.y
+                cvCircle(frame, cvPoint(int(f.x), int(f.y)), 3, CV_RGB(0, 0, 255), 1)
+
+            cvResetImageROI(self.gray)
+            cvResetImageROI(self.eigs)
+            cvResetImageROI(self.temp)
+
+            self.state = 'track_face'
+
+    def state_track_face(self, frame):
+        (x, y, height, size) = self.history[-1]
+        best = None
+
+        for face in cvHaarDetectObjects(self.small, self.cascade, self.storage, 1.1, 2, CV_HAAR_DO_CANNY_PRUNING, cvSize(30, 30)):
+            r = CvRect(int(face.x*self.scale), int(face.y*self.scale), int(face.width*self.scale), int(face.height*self.scale))
+            if not best or abs(r.height-height) < abs(best.height-height):
+                best = r
+            cvRectangle(frame, CvPoint(r.x, r.y), CvPoint(r.x+r.width, r.y+r.height), CV_RGB(0, 0, 255), 3, 8, 0)
+
+        if best:
+            cvRectangle(frame, CvPoint(best.x, best.y), CvPoint(best.x+best.width, best.y+best.height), CV_RGB(0, 255, 0), 3, 8, 0)
+            self.history = self.history[1:] + (self.rect_to_params(best),)
+
+            features, status = cvCalcOpticalFlowPyrLK(self.prev, self.gray, self.pyr_a, self.pyr_b, self.features, None, None, CvSize(50, 50), 3, None, None, cvTermCriteria(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS, 10, 0.03), CV_LKFLOW_PYR_A_READY)
+            new_features = [x for x in features]
+            dx, dy, max_x, max_y, min_x, min_y = 0, 0, 0, 0, 1000, 1000
+            for i in range(0, len(features)):
+                if ord(status[i]) == 0:
+                    print "lost feature:", i
+                    del new_features[i]
+                else:
+                    dx = dx + (features[i].x - self.features[i].x)
+                    dy = dy + (features[i].y - self.features[i].y)
+                    max_x = max(max_x, features[i].x)
+                    max_y = max(max_y, features[i].y)
+                    min_x = min(min_x, features[i].x)
+                    min_y = min(min_y, features[i].y)
+                    cvCircle(frame, cvPoint(int(features[i].x), int(features[i].y)), 3, CV_RGB(0, 0, 255), 1)
+            if len(new_features) > 0:
+                dx, dy = dx / len(new_features), dy / len(new_features)
+            self.features = new_features
+
+            print "estimated motion:", dx, dy
+
+            if len(self.features) < 5:
+                self.state = 'find_face'
+        else:
+            self.state = 'find_face'
+
     def main(self):
         while True:
             frame = cvQueryFrame(self.capture)
@@ -111,71 +180,12 @@ class FaceTracking:
             cvClearMemStorage(self.storage)
 
             if self.state == 'find_face':
-                best = None
-
-                for face in cvHaarDetectObjects(self.small, self.cascade, self.storage, 1.1, 2, CV_HAAR_DO_CANNY_PRUNING, cvSize(30, 30)):
-                    r = CvRect(int(face.x*self.scale), int(face.y*self.scale), int(face.width*self.scale), int(face.height*self.scale))
-                    if not best or r.height > best.height:
-                        best = r
-                    cvRectangle(frame, CvPoint(r.x, r.y), CvPoint(r.x+r.width, r.y+r.height), CV_RGB(255, 0, 0), 3, 8, 0)
-
-                if best:
-                    self.history = self.history[1:] + (self.rect_to_params(best),)
-                    self.state = 'track_face'
-
-                    cvSetImageROI(self.gray, best)
-                    cvSetImageROI(self.eigs, best)
-                    cvSetImageROI(self.temp, best)
-
-                    self.features = [x for x in cvGoodFeaturesToTrack(self.gray, self.eigs, self.temp, None, 20, 0.02, 4.0, use_harris=False)]
-                    print "identified features:", len(self.features)
-                    for f in self.features:
-                        f.x = f.x + best.x
-                        f.y = f.y + best.y
-                        cvCircle(frame, cvPoint(int(f.x), int(f.y)), 3, CV_RGB(0, 0, 255), 1)
-
-                    cvResetImageROI(self.gray)
-                    cvResetImageROI(self.eigs)
-                    cvResetImageROI(self.temp)
-
+                self.state_find_face(frame)
             elif self.state == 'track_face':
-                (x, y, height, size) = self.history[-1]
-                best = None
-
-                for face in cvHaarDetectObjects(self.small, self.cascade, self.storage, 1.1, 2, CV_HAAR_DO_CANNY_PRUNING, cvSize(30, 30)):
-                    r = CvRect(int(face.x*self.scale), int(face.y*self.scale), int(face.width*self.scale), int(face.height*self.scale))
-                    if not best or abs(r.height-height) < abs(best.height-height):
-                        best = r
-                    cvRectangle(frame, CvPoint(r.x, r.y), CvPoint(r.x+r.width, r.y+r.height), CV_RGB(0, 0, 255), 3, 8, 0)
-
-                if best:
-                    cvRectangle(frame, CvPoint(best.x, best.y), CvPoint(best.x+best.width, best.y+best.height), CV_RGB(0, 255, 0), 3, 8, 0)
-                    self.history = self.history[1:] + (self.rect_to_params(best),)
-
-                    features, status = cvCalcOpticalFlowPyrLK(self.prev, self.gray, self.pyra, self.pyrb, self.features, None, None, CvSize(50, 50), 3, None, None, cvTermCriteria(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS, 10, 0.03), CV_LKFLOW_PYR_A_READY)
-                    new_features = [x for x in features]
-                    dx, dy = 0, 0
-                    for i in range(0, len(features)):
-                        if ord(status[i]) == 0:
-                            print "lost feature:", i
-                            del new_features[i]
-                        else:
-                            dx = dx + (features[i].x - self.features[i].x)
-                            dy = dy + (features[i].y - self.features[i].y)
-                            cvCircle(frame, cvPoint(int(features[i].x), int(features[i].y)), 3, CV_RGB(0, 0, 255), 1)
-                    if len(new_features) > 0:
-                        dx, dy = dx / len(new_features), dy / len(new_features)
-                    self.features = new_features
-
-                    print "estimated motion:", dx, dy
-
-                    if len(self.features) < 5:
-                        self.state = 'find_face'
-                else:
-                    self.state = 'find_face'
+                self.state_track_face(frame)
 
             cvCopy(self.gray, self.prev)
-            self.pyra, self.pyrb = self.pyrb, self.pyra
+            self.pyr_a, self.pyr_b = self.pyr_b, self.pyr_a
 
             cvShowImage('small', self.small)
             cvShowImage('frame', frame)
