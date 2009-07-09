15bf46f95a2ca1a60d0b3fd66002e206d73e0076
Justin Dubs <jtdubs@gmail.com>
2009-07-09 01:12:09 -0400

    mirror skin image left/right

--- skin.py
+++ skin.py
@@ -10,6 +10,7 @@ def main():
     capture         = cvCaptureFromCAM(0)
     frame           = cvQueryFrame(capture)
     size            = cvSize(frame.width, frame.height)
+    copy            = cvCreateImage(size, 8, 3)
     r               = cvCreateImage(size, 8, 1)
     g               = cvCreateImage(size, 8, 1)
     b               = cvCreateImage(size, 8, 1)
@@ -28,16 +29,17 @@ def main():
 
     while True:
         frame = cvQueryFrame(capture)
-        cvShowImage('frame', frame)
+        cvFlip(frame, copy, 1)
+        cvShowImage('frame', copy)
 
         # build normalized rgb image
-        cvSplit(frame, b, g, r)
+        cvSplit(copy, b, g, r)
         cvAddWeighted(r, 1/3.0, g, 1/3.0, 0.0, sum)
         cvAddWeighted(sum, 1.0, b, 1/3.0, 0.0, sum)
-        cvSetImageCOI(frame, 1); cvDiv(r, sum, r, 255/3)
-        cvSetImageCOI(frame, 2); cvDiv(g, sum, g, 255/3)
-        cvSetImageCOI(frame, 3); cvDiv(b, sum, b, 255/3)
-        cvSetImageCOI(frame, 0)
+        cvSetImageCOI(copy, 1); cvDiv(r, sum, r, 255/3)
+        cvSetImageCOI(copy, 2); cvDiv(g, sum, g, 255/3)
+        cvSetImageCOI(copy, 3); cvDiv(b, sum, b, 255/3)
+        cvSetImageCOI(copy, 0)
         cvMerge(b, g, r, None, bgr)
         cvShowImage('rgb', bgr)
 
@@ -69,7 +71,7 @@ def main():
 
         # apply mask to frame
         cvSetZero(skin)
-        cvCopy(frame, skin, mask)
+        cvCopy(copy, skin, mask)
         cvShowImage('skin', skin)
 
         k = cvWaitKey(10)
