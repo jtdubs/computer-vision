c9cd4dee8745ca247592c5c17121c75c317d826e
Justin Dubs <jtdubs@gmail.com>
2009-07-17 19:48:48 -0400

    logic in place to locate multiple decals in object-space

--- decals.py
+++ decals.py
@@ -38,14 +38,31 @@ def decals(polys):
                 yield (decal, inner_polys[0].total)
 
 def main():
-    capture         = cvCaptureFromCAM(0)
-    frame           = cvQueryFrame(capture)
-    size            = cvSize(frame.width, frame.height)
-    copy            = cvCreateImage(size, 8, 3)
-    gray            = cvCreateImage(size, 8, 1)
-    edges           = cvCreateImage(size, 8, 1)
-    contour_storage = cvCreateMemStorage(0)
-    quad_storage    = cvCreateMemStorage(0)
+    capture            = cvCaptureFromCAM(0)
+    frame              = cvQueryFrame(capture)
+    size               = cvSize(frame.width, frame.height)
+    copy               = cvCreateImage(size, 8, 3)
+    gray               = cvCreateImage(size, 8, 1)
+    edges              = cvCreateImage(size, 8, 1)
+    storage            = cvCreateMemStorage(0)
+    decal_mat          = cvCreateMat(4, 3, CV_32FC1)
+    image_mat          = cvCreateMat(4, 2, CV_32FC1)
+    intrinsic          = cvCreateMat(3, 3, CV_32FC1)
+    distortion         = cvCreateMat(1, 4, CV_32FC1)
+    rotation           = cvCreateMat(1, 3, CV_32FC1)
+    rotation_matrix    = cvCreateMat(3, 3, CV_32FC1)
+    gl_rotation_matrix = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]
+    translation        = cvCreateMat(1, 3, CV_32FC1)
+
+    ci = [682.80694580078125, 0.0, 331.3616943359375, 0.0, 631.85980224609375, 210.08140563964844, 0.0, 0.0, 1.0]
+    cd = [0.34955242276191711, -0.70636618137359619, -0.013230122625827789, 0.0091487327590584755]
+    for x in range(0, 3):
+        for y in range(0, 3):
+            intrinsic[x, y] = ci[(x*3)+y]
+    for x in range(0, 4):
+        distortion[0, x] = cd[x]
+    for i, (x, y) in enumerate([(0, 0), (0, 1), (1, 1), (1, 0)]):
+        decal_mat[i,0], decal_mat[i,1], decal_mat[i,2] = x, y, 0
 
     cvNamedWindow('edges', 1)
     cvNamedWindow('contours', 1)
@@ -58,14 +75,23 @@ def main():
         cvDilate(edges, edges, iterations=1)
         cvShowImage('edges', edges)
 
-        ps = list(polys(contours(edges, contour_storage)))
+        ps = list(polys(contours(edges, storage)))
         # for poly in ps:
         #     cvDrawContours(copy, poly, CV_RGB(255,0,0), CV_RGB(255,0,0), 0, 2, 8)
         for (decal, n) in decals(ps):
-            if n == 3:
-                cvDrawContours(copy, decal, CV_RGB(0,255,0), CV_RGB(0,255,0), 0, 2, 8)
-            elif n == 4:
-                cvDrawContours(copy, decal, CV_RGB(0,0,255), CV_RGB(0,0,255), 0, 2, 8)
+            color = CV_RGB(0,255,0) if n == 3 else CV_RGB(0,0,255)
+            cvDrawContours(copy, decal, color, color, 0, 2, 8)
+
+            ps = [CvPoint2D32f(p.x, p.y) for p in decal.asarray(CvPoint)]
+            cvFindCornerSubPix(gray, ps, CvSize(2, 2), CvSize(-1, -1), cvTermCriteria(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS, 100, 0.01))
+            for i, p in enumerate(ps):
+                image_mat[i,0], image_mat[i,1] = p.x, p.y
+
+            cvFindExtrinsicCameraParams2(decal_mat, image_mat, intrinsic, distortion, rotation, translation)
+            cvRodrigues2(rotation, rotation_matrix)
+            for x in range(0,3):
+                for y in range(0,3):
+                    gl_rotation_matrix[(y*4)+x] = rotation_matrix[x,y]
 
         cvShowImage('contours', copy)
 
