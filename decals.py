5cd430fa0e841c7cffb43f8ed0a63f5bbcb57d66
Justin Dubs <jtdubs@gmail.com>
2009-07-17 18:50:18 -0400

    generalize to quads containing single polygons

--- decals.py
+++ decals.py
@@ -13,29 +13,29 @@ def contours(img, storage):
         contour = cvFindNextContour(scanner)
     del scanner
 
-def quads(contours, storage):
+def polys(contours):
     for contour in contours:
         hole = contour.flags & CV_SEQ_FLAG_HOLE
-        if not hole:
-            cvClearMemStorage(storage)
-            quad = cvApproxPoly(contour, sizeof(CvContour), storage, CV_POLY_APPROX_DP, contour.rect.width/10)
-            if quad.total == 4 and cvCheckContourConvexity(quad):
-                yield contour
+        if hole:
+            poly = cvApproxPoly(contour, sizeof(CvContour), None, CV_POLY_APPROX_DP, contour.rect.width/10)
+            if cvCheckContourConvexity(poly):
+                yield poly
 
-def decals(quads):
-    for decal in quads:
-        inside_count = 0
-        for marker in quads:
-            if pointer(marker) <> pointer(decal):
-                inside = True
-                for pt in marker.asarray(CvPoint):
-                    pt = CvPoint2D32f(pt.x, pt.y)
-                    if cvPointPolygonTest(decal, pt, 0) <= 0:
-                        inside = False
-                if inside:
-                    inside_count = inside_count + 1
-        if inside_count == 1:
-            yield decal
+def decals(polys):
+    for decal in polys:
+        if decal.total == 4:
+            inside_count = 0
+            for marker in polys:
+                if pointer(marker) <> pointer(decal):
+                    inside = True
+                    for pt in marker.asarray(CvPoint):
+                        pt = CvPoint2D32f(pt.x, pt.y)
+                        if cvPointPolygonTest(decal, pt, 0) <= 0:
+                            inside = False
+                    if inside:
+                        inside_count = inside_count + 1
+            if inside_count == 1:
+                yield decal
 
 def main():
     capture         = cvCaptureFromCAM(0)
@@ -58,32 +58,16 @@ def main():
         cvDilate(edges, edges, iterations=1)
         cvShowImage('edges', edges)
 
-        qs = list(quads(contours(edges, contour_storage), quad_storage))
-        # for quad in qs:
-        #     cvDrawContours(copy, quad, CV_RGB(255,0,0), CV_RGB(255,0,0), 0, 2, 8)
-        for decal in decals(qs):
+        ps = list(polys(contours(edges, contour_storage)))
+        # for poly in ps:
+        #     cvDrawContours(copy, poly, CV_RGB(255,0,0), CV_RGB(255,0,0), 0, 2, 8)
+        for decal in decals(ps):
             cvDrawContours(copy, decal, CV_RGB(0,255,0), CV_RGB(0,255,0), 0, 2, 8)
 
         cvShowImage('contours', copy)
 
         k = cvWaitKey(10)
-        if   k == 27:       break
+        if  k == 27: break
 
 if __name__ == '__main__':
     main()
-
-#             marker = pointee(cast(contour.v_next, CvContour_p))
-#             if marker is not None:
-#                 cvClearMemStorage(storage)
-#                 marker_poly = cvApproxPoly(marker,      sizeof(CvContour), storage, CV_POLY_APPROX_DP, 7)
-#                 marker_poly = cvApproxPoly(marker_poly, sizeof(CvContour), None,    CV_POLY_APPROX_DP, 7)
-#                 if marker_poly.total == 4 and cvCheckContourConvexity(marker_poly):
-#                     yield contour
-
-#                        hole = pointee(cast(marker.v_next, CvContour_p))
-#                        cvClearMemStorage(storage)
-#                        hole_poly = cvApproxPoly(hole,      sizeof(CvContour), storage, CV_POLY_APPROX_DP, 7)
-#                        hole_poly = cvApproxPoly(hole_poly, sizeof(CvContour), None,    CV_POLY_APPROX_DP, 7)
-#                        if hole_poly.total == 4 and cvCheckContourConvexity(hole_poly):
-#                            yield contour
-
